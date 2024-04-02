ae31c484bb19d5d2ea5805d1ea22ab1169c1a877
marcosmadeira34 <mmadeirasilva5@gmail.com>
2024-04-02 17:01:51 -0300

    modified paths

--- src/main_refactory.py
+++ src/main_refactory.py
@@ -9,13 +9,32 @@ transform_pipeline = TransformPipeline()
 load_pipeline = LoadPipeline()
 consolidator = ConsolidatePipeline()
 
+# DIRETÓRIO DE ENTRADA DOS ARQUIVOS EXTRATORES (EXTRACTION)
+extractor_file_path = r"/home/administrator/WindowsShare/01 - FATURAMENTO/00-EXTRATOR_PEDIDOS_DE_CLIENTES" # EXTRATOR
+
+# DIRETÓRIOS DE SAÍDA DOS ARQUIVOS CRIADOS (LOADING)
+batch_totvs_path = r'/home/administrator/WindowsShare/01 - FATURAMENTO/01 - CLIENTES - CONTROLE - 2024 TOTVS' # CRIARÁ AS PASTA AQUI
+
+# DIRETÓRIO DE TRATAMENTO DOS ARQUIVOS (TRANSFORMATION)
+data_raw_path = r'/home/administrator/WindowsShare/01 - FATURAMENTO/02 - DATA_RAW' # NOVOS PEDIDOS IDENTIFICADOS NO EXTRATOR
+source_directory = r'/home/administrator/WindowsShare/01 - FATURAMENTO/02 - DATA_RAW' # DIRETÓRIO DE ORIGEM DOS PEDIDOS
+target_directory = r'/home/administrator/WindowsShare/01 - FATURAMENTO/01 - CLIENTES - CONTROLE - 2024 TOTVS' # DIRETÓRIO DE DESTINO DOS PEDIDOS
+
+# DIRETÓRIO DE ARQUIVOS PROCESSADOS (DRAFT)
+process_files = r'/home/administrator/WindowsShare/01 - FATURAMENTO/04 - EXTRATORES PROCESSADOS'
+
+file_path_error = r'/home/administrator/WindowsShare/01 - FATURAMENTO/05 - EXTRATORES COM ERROS' # ARQUIVOS COM ERROS NO EXTRATOR
+
+# DIRETÓRIOS AUXILIARES (SANDBOX)
+output_merge_path = r'C:/DataWare/data/consolidated_files/consolidated_validated/MERGE_RELATÓRIO_FINAL' # RELATÓRIO FINAL 
+invoiced_orders = r'C:/DataWare/data/consolidated_files/consolidated_validated/PEDIDOS_FATURADOS' # PEDIDOS FATURADOS NO BANCO DE DADOS
 
 
-extractor_file_path = r"C:\Users\marcos.silvaext\Documents\01 - INPUT_DATA"
-data_raw_path = r"C:\Users\marcos.silvaext\Documents\02 - DATA_RAW"
-file_path_error = r"C:\Users\marcos.silvaext\Documents\05 - EXTRATORES COM ERROS"
-report_path = r"C:\Users\marcos.silvaext\Documents\04 - REPORTS"
-duplicate_file_path = r"C:\Users\marcos.silvaext\Documents\06 - ARQUIVOS DUPLICADOS"
+# extractor_file_path = r"C:\Users\marcos.silvaext\Documents\01 - INPUT_DATA"
+# data_raw_path = r"C:\Users\marcos.silvaext\Documents\02 - DATA_RAW"
+# file_path_error = r"C:\Users\marcos.silvaext\Documents\05 - EXTRATORES COM ERROS"
+# target_directory = r"C:\Users\marcos.silvaext\Documents\04 - REPORTS"
+# duplicate_file_path = r"C:\Users\marcos.silvaext\Documents\06 - ARQUIVOS DUPLICADOS"
 
 column_name = 'Pedido Faturamento'
 
@@ -49,11 +68,11 @@ while True:
 
         # print('Etapa de formatação finalizada com sucesso!')
 
-        transformated_files = load_pipeline.move_files_to_month_subfolder(data_raw_path, report_path)
+        transformated_files = load_pipeline.move_files_to_month_subfolder(data_raw_path, target_directory)
         
-        """ files_consolidated = consolidator.list_files_to_consolidate(report_path)
+        """ files_consolidated = consolidator.list_files_to_consolidate(target_directory)
         
         if files_consolidated: """
-        consolidator.merge_excel_reports(report_path)
-        # transform_pipeline.format_styles_report_sheet(files_consolidated, report_path)
+        consolidator.merge_excel_reports(target_directory)
+        # transform_pipeline.format_styles_report_sheet(files_consolidated, target_directory)
         print('Etapa de consolidação finalizada com sucesso!')
